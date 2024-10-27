c9e8abfc2244517d2b0632988587555758ef56f4
Jay Prakash <jayp7808@gmail.com>
2024-10-27 22:51:36 +0530

    adding date

--- src/pages/Dashboard/DashBoard.jsx
+++ src/pages/Dashboard/DashBoard.jsx
@@ -84,7 +84,7 @@ const EventDashboard = () => {
                                     <h3 className="event-title">{event.title}</h3>
                                     <p className="event-description">{event.description}</p>
                                     <p className="event-location">{event.location}</p>
-                                    <p>DATE: {event.data}, Time: {event.time}</p>
+                                    <p>DATE: {event.date}, Time: {event.time}</p>
                                     
                                     <p className="event-ticket-price">Price: {event.ticketPrice}</p>
                                     
