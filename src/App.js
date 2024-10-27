49d0a3ec5575c787ea5bacb6927cd12f182eb295
Jay Prakash <jayp7808@gmail.com>
2024-10-27 21:24:45 +0530

    Add MainPage.jsx

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import EditEvent from './components/editEvent/EditEvent';
 import PaymentPage from './components/paymentPage/PaymentPage';
 import TicketPage from './components/ticket/Ticket';
 import AttendeesPage from './components/attendees/Attendees';
+import mainPage from './pages/mainPage/mainPage';
 
 const App = () => {
     return (
@@ -18,6 +19,7 @@ const App = () => {
             <Router>
                 <div className="App">
                     <Routes>
+                        <Route path="/" element={<mainPage/>} />
                         <Route path="/register" element={<Register />} />
                         <Route path="/login" element={<Login />} />
                         {/* Use ProtectedRoute to guard the dashboard */}
---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Register from '../../components/register/Register'
+import Login from '../../components/login/Login'
+
+const mainPage = () => {
+  return (
+    <div>
+      <Register/>
+      <Login/>
+    </div>
+  )
+}
+
+export default mainPage
