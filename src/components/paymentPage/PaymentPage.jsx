59428b621650a43bd012d5f219d0b26341d1b441
Jay Prakash <jayp7808@gmail.com>
2024-10-28 00:19:17 +0530

    change in paymentpage dotenv import remove

--- src/components/paymentPage/PaymentPage.jsx
+++ src/components/paymentPage/PaymentPage.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import dotenv from 'dotenv'
-
-dotenv.config();
 
 import { useNavigate, useParams } from 'react-router-dom';
 import './paymentPage.css';
