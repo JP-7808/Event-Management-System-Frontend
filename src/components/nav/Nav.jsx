757938b24199685f94e3fff19485ab895a5b56d0
Jay Prakash <jayp7808@gmail.com>
2024-11-01 19:57:09 +0530

    Change in logout

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import Cookies from 'js-cookie';
+import axios from 'axios';
 import './nav.css'; // Import the external CSS file
 
 const Navbar = () => {
