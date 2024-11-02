5120ab175bb77c36216084c5c0600e638c2d167f
Jay Prakash <jayp7808@gmail.com>
2024-11-02 19:41:10 +0530

    Added Register page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AttendeesPage from './components/attendees/Attendees';
 import MainPage from './pages/mainPage/MainPage';
 import Login from './components/login/Login';
 import { AuthContextProvider } from './context/AuthContext';
+import Register from './components/register/Register';
 
 const App = () => {
     return (
@@ -19,6 +20,7 @@ const App = () => {
                             <Route path="/" element={<MainPage/>} />
                             {/* Use ProtectedRoute to guard the dashboard */}
                             <Route path='/login' element={<Login/>}/>
+                            <Route path='/register' element={<Register/>} />
                             <Route path='/dashboard' element={<DashBoard/>} />
                             <Route path="/eventCreation" element={<EventCreation/>} />
 
